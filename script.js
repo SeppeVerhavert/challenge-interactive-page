473a10209d554c7ce59eb4174ef862314b781b99
seppe_verhavert <seppe.verhavert@gmail.com>
2019-08-01 11:00:15 +0200

    add buttons to js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 let carousel = document.getElementsByClassName("photo");
+let buttonLeft = document.getElementById("button-1");
+let buttonRight = document.getElementById("button-2");
+
+buttonLeft.addEventListener('click', carouselSlideLeft);
+buttonRight.addEventListener('click', carouselSlideRight);
+
+function carouselSlideLeft() {
+    console.log("left");
+}
+
+function carouselSlideRight() {
+    console.log("Right");
+}
+
+
+
+
