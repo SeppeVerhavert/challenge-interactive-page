959b41fc8164fed34645eeb12b2402c0cf25be84
seppe_verhavert <seppe.verhavert@gmail.com>
2019-08-01 12:03:50 +0200

    fix carousel slider bug

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let buttonRight = document.getElementById("button-2");
 let images = ['url("assets/carousel-stage.jpg")', 'url("assets/carousel-band-1.jpg")', 'url("assets/carousel-band-2.jpg")', 'url("assets/carousel-band-3.jpg")'];
 let i = 0;
 
+let carouselInterval = window.onload = setInterval(carouselSlideRight, 3000);
+
 buttonLeft.addEventListener('click', carouselSlideLeft);
 buttonRight.addEventListener('click', carouselSlideRight);
 
@@ -26,9 +28,6 @@ function carouselSlideRight() {
     carouselInterval;
 }
 
-function CarouselInterval() {
-    let interval = setInterval(carouselSlideRight, 3000);
-}
 
 
 
