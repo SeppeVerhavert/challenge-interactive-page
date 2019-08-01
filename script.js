c6f0edcd601ee52d0654cc4bb29181023cd1068d
seppe_verhavert <seppe.verhavert@gmail.com>
2019-08-01 12:11:18 +0200

    start tabs

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+//-------------------------------------     VARIABLES    --------------------------------------------//
+
 let carousel = document.getElementsByClassName("photo")[0];
 let buttonLeft = document.getElementById("button-1");
 let buttonRight = document.getElementById("button-2");
+
+let tabSummary = document.getElementById("summary");
+let tabInfo = document.getElementById("info");
+let tabContact = document.getElementById("contact-form");
 
 let images = ['url("assets/carousel-stage.jpg")', 'url("assets/carousel-band-1.jpg")', 'url("assets/carousel-band-2.jpg")', 'url("assets/carousel-band-3.jpg")'];
 let i = 0;
@@ -9,6 +15,8 @@ let carouselInterval = window.onload = setInterval(carouselSlideRight, 3000);
 
 buttonLeft.addEventListener('click', carouselSlideLeft);
 buttonRight.addEventListener('click', carouselSlideRight);
+
+//-------------------------------------     CAROUSEL    --------------------------------------------//
 
 function carouselSlideLeft() {
     if (i < 1) {
@@ -27,6 +35,9 @@ function carouselSlideRight() {
     carousel.style.backgroundImage = images[i];
     carouselInterval;
 }
+
+//-------------------------------------     TABS    --------------------------------------------//
+
 
 
 
