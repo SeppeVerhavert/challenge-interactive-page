eaac8960a5460ff57629121f807e341b436ac3d0
seppe_verhavert <seppe.verhavert@gmail.com>
2019-08-01 13:25:37 +0200

    add shgow tabs on click

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ let tabSummary = document.getElementById("summary");
 let tabInfo = document.getElementById("info");
 let tabContact = document.getElementById("contact-form");
 
-let summaryBtn = document.getElementById("");
-let infoBtn = document.getElementById("");
-let contactBtn = document.getElementById("");
+let summaryBtn = document.getElementById("summary-button");
+let infoBtn = document.getElementById("info-button");
+let contactBtn = document.getElementById("contact-button");
 
 summaryBtn.addEventListener('click', showSummary);
 infoBtn.addEventListener('click', showInfo);
@@ -46,14 +46,25 @@ function carouselSlideRight() {
 
 //-------------------------------------     TABS    --------------------------------------------//
 
-function showSummary() {
+showSummary();
 
+function showSummary() {
+    tabSummary.style.display = "block";
+    tabInfo.style.display = "none";
+    tabContact.style.display = "none";
+    console.log("summary");
 }
 
 function showInfo() {
-
+    tabSummary.style.display = "none";
+    tabInfo.style.display = "block";
+    tabContact.style.display = "none";
+    console.log("info");
 }
 
 function showContact() {
-    
+    tabSummary.style.display = "none";
+    tabInfo.style.display = "none";
+    tabContact.style.display = "block";
+    console.log("contact");
 }
